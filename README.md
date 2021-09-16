c127d02f28039a9d4dc6a411f1bbb940257fb07b
zunairzaman <86424444+zunairzaman@users.noreply.github.com>
2021-09-16 14:46:36 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Crush-the-zombies--
+# zombie-crush-boilerplate
+boilerplate for crush the zombie game
---
+++ sketch.js
@@ -0,0 +1,37 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+var stones = []
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+  frameRate(80);
+
+  Matter.Composite.add(bridge.body,jointPoint);
+  jointLink = new Link(bridge, jointPoint);
+
+  for (var i = 0; i <= 8; i++) {
+    var x = random(width / 2 - 200, width / 2 + 300);
+    var y = random(-10, 140);
+    var stone = new Stone(x, y, 80, 80);
+    stones.push(stone);
+  }
+}
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+  for (var stone of stones) {
+    stone.show();
+}
+}
